0b4fb1fdc4d832e042674dc2a3ebc9c8d7479c82
Sergey Golubev <snowman@testing2.searchanise>
2023-06-08 18:09:11 +0400

    se-11570 Add compatibility with php 8.2

--- src/Google/Service/Analytics.php
+++ src/Google/Service/Analytics.php
@@ -70,6 +70,7 @@ class Google_Service_Analytics extends Google_Service
   public $management_webpropertyUserLinks;
   public $metadata_columns;
   public $provisioning;
+  public $serviceName;
   
 
   /**
